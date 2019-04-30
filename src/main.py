df8080b8ad2e118b82ab0be4e5526e2551fb91ee
2ToThe10th <grib.a@phystech.edu>
2019-04-30 20:39:21 +0300

    config file added

--- src/main.py
+++ src/main.py
@@ -6,15 +6,6 @@ import string
 import send_alarm_clock
 import time
 
-TOKEN = "749160029:AAFuIWuEjGMMLv7X66vMGch1u3ZCqP1RLtk"
-SSL_CERT = "cert.pem"
-SSL_KEY = "key.pem"
-HOST = "217.73.88.161"
-PORT = 8443
-DB_NAME = "alarmclock"
-DB_USER = "alarmclock"
-PASSWORD = "alarmclockpasswd"
-SECRET_KEY = "rdghea1jbMRzBQrgYqwCop6JQz0A2VHCjnpVJSkLxX6oYWzpH4"
 TELEGRAM_PATH = ''.join(secrets.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(50))
 
 db = postgresql.Postgresql(DB_NAME, DB_USER, PASSWORD)
